b5fc6ecfa42253b28ee66d26ed31fd780639b6b2
amittiwari5446 <amit5446tiwari@gmail.com>
2026-09-10 16:33:32 +0530

    PartitionBy Completed

--- src/main/java/org/example/partitionByConcept/PartitionByDemo.java
+++ src/main/java/org/example/partitionByConcept/PartitionByDemo.java
@@ -1,7 +1,9 @@
 package org.example.partitionByConcept;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class PartitionByDemo {
@@ -23,14 +25,14 @@ public class PartitionByDemo {
         System.out.println("Even odd partition: "+evenOdd);
 
         //for custom objects
-        record Employee(String name, String department, double salary, int age) {}
+        record Employee(String name, String department, double salary, int age, String gender) {}
 
         List<Employee> employees = List.of(
-                new Employee("Alice", "IT", 90000, 28),
-                new Employee("Bob", "HR", 65000, 35),
-                new Employee("Charlie", "IT", 110000, 32),
-                new Employee("Diana", "Finance", 75000, 29),
-                new Employee("Eve", "IT", 85000, 26)
+                new Employee("Alice", "IT", 90000, 28, "F"),
+                new Employee("Bob", "HR", 65000, 35,"M"),
+                new Employee("Charlie", "IT", 110000, 32,"M"),
+                new Employee("Diana", "Finance", 75000, 29,"F"),
+                new Employee("Eve", "IT", 85000, 26,"F")
         );
 
         Map<Boolean,List<Employee>> byHighSalary=employees.stream()
@@ -54,6 +56,30 @@ public class PartitionByDemo {
         System.out.println("Average In Partition: "+averageInPartition);
 
         //get only names in each partition
+        Map<Boolean, List<String>> onlyNames=employees.stream()
+                .collect(Collectors.partitioningBy(
+                        emp->emp.department.equals("IT"),
+                        Collectors.mapping(emp->emp.name,Collectors.toList())
+                ));
+        System.out.println("Only names: "+onlyNames);
 
+
+        //PartitionBy + minBy/MaxBy
+        Map<Boolean, Optional<Employee>> maxByPartition=employees.stream()
+                .collect(Collectors.partitioningBy(
+                        emp->emp.department.equals("IT"),
+                        Collectors.maxBy(Comparator.comparingDouble(Employee::salary))
+                ));
+        System.out.println("MaxBy Partition: "+maxByPartition);
+
+        //first Partition by "IT" or "Non-IT" then according to geneder
+        Map<Boolean, Map<String,List<Employee>>> partitionThenGroupingBy=employees.stream()
+                .collect(Collectors.partitioningBy(
+                        emp->emp.department.equals("IT"),
+                        Collectors.groupingBy(Employee::gender,Collectors.toList())
+                ));
+        System.out.println("partition by 'IT' then group by gender: "+partitionThenGroupingBy);
+
+        
     }
 }
